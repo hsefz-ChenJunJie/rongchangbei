dbd0ceb0308ae70d119576c57a1dc59075961670
ECJKropas <3462695908@qq.com>
2025-08-13 22:10:46 +0800

    Add tts to local project

--- frontend_flutter/ai_sound_agent/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_flutter/ai_sound_agent/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import flutter_tts
 import path_provider_foundation
 import record_macos
 import shared_preferences_foundation
@@ -14,6 +15,7 @@ import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
