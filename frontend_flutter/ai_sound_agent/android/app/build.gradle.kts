271ab5cda3365d9b6c54795af0740dd6fc51c9d3
ECJKropas <3462695908@qq.com>
2025-08-16 22:33:13 +0800

    Update Version num

--- frontend_flutter/ai_sound_agent/android/app/build.gradle.kts
+++ frontend_flutter/ai_sound_agent/android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.efzzz.ai_sound_agent"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.2"
+        versionCode = 6
+        versionName = "0.1.3"
     }
 
     buildTypes {
